a86f4b82e793ff0273320702a1c91a49f39c6ded
= <rdenman@Robs-MacBook-Air.local>
2019-10-23 18:21:30 -0500

    Updates

---
+++ precache-manifest.1576b4eb67355275d1d20e8370d09bf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c9a4127c763b298f7fbe9a7cbb9307",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8015510ad9ac5d8d391c",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "091bc5588daa8429eb38",
+    "url": "/robofriends/static/css/main.2689414d.chunk.css"
+  },
+  {
+    "revision": "8015510ad9ac5d8d391c",
+    "url": "/robofriends/static/js/2.916d05e9.chunk.js"
+  },
+  {
+    "revision": "091bc5588daa8429eb38",
+    "url": "/robofriends/static/js/main.9036b606.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
